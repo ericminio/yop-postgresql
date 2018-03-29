6ca49b90c95d12308e46e7bfec7873af45a7b98c
Eric Mignot <eric.mignot@gmail.com>
2018-03-29 08:26:15 -0700

    errors bubble

--- demo/execute.spec.js
+++ demo/execute.spec.js
@@ -50,7 +50,7 @@ describe('execute', () => {
         });
     });
     it('let sql errors bubble', (done)=>{
-        execute('select from unknown', [], (rows, error)=>{
+        execute('select * from unknown', [], (rows, error)=>{
             expect(error.message).to.contain('"unknown" does not exist');
             done();
         });
@@ -69,6 +69,93 @@ describe('execute', () => {
     it('defaults error to undefined', (done)=>{
         execute('select current_user', [], (rows, error)=>{
             expect(error).to.equal(undefined);
+            done();
+        });
+    });
+    it('allows to use strings as statements', (done)=>{
+        var all = [
+            'create table if not exists greatness(id int, name varchar)',
+            'truncate table greatness',
+            'insert into greatness(id, name) values(1, \'liberty\')',
+            'select name from greatness'
+        ];
+        execute(all, (rows)=>{
+            expect(rows.length).to.equal(1);
+            expect(rows[0].name).to.equal('liberty');
+            done();
+        });
+    });
+    it('stops after first error in collection', (done)=>{
+        var all = [
+            'create table if not exists greatness(id int, name varchar)',
+            'truncate table greatness',
+            'create tableifnotexists greatness(id int, name varchar)',
+            'insert into greatness(id, name) values(1, \'liberty\')',
+            'select name from greatness'
+        ];
+        execute(all, (rows, error)=>{
+            expect(error.message).to.contain('syntax error at or near "tableifnotexists"');
+            expect(rows).to.deep.equal([]);
+
+            setTimeout(()=> {
+                execute('select name from greatness', [], (rows)=>{
+                    expect(rows).to.deep.equal([]);
+                    done();
+                });
+            }, 300);            
+        });
+    });
+    it('accepts different formats in the same collection', (done)=>{
+        var all = [
+            'create table if not exists greatness(id int, name varchar)',
+            'truncate table greatness',
+            { sql: 'insert into greatness(id, name) values($1, $2)', params:[1, 'liberty'] },
+            { sql: 'insert into greatness(id, name) values($1, $2)', params:[2, 'equality'] },
+            { sql: 'insert into greatness(id, name) values($1, $2)', params:[3, 'fraternity'] },
+            'select name from greatness'
+        ];
+        execute(all, (rows)=>{
+            expect(rows.length).to.equal(3);
+            expect(rows[2].name).to.equal('fraternity');
+            done();
+        });
+    });
+    it('stops also when first statement fails', (done)=>{
+        var all = [
+            'create tableifnotexists greatness(id int, name varchar)',
+            'create table if not exists greatness(id int, name varchar)',
+            'truncate table greatness',
+            'insert into greatness(id, name) values(1, \'liberty\')',
+            'select name from greatness'
+        ];
+        execute(all, (rows, error)=>{
+            expect(rows).to.deep.equal([]);
+            expect(error.message).to.contain('syntax error at or near "tableifnotexists"');
+
+            setTimeout(()=> {
+                var all = [
+                    'create table if not exists greatness(id int, name varchar)',
+                    'truncate table greatness',
+                    'insert into greatness(id, name) values(1, \'liberty\')',
+                    'select name from greatness'
+                ];
+                execute(all, (rows)=>{
+                    expect(rows.length).to.equal(1);
+                    expect(rows[0].name).to.equal('liberty');
+                    done();
+                });
+            }, 300);            
+        });
+    });
+    it('accepts a single statement in the collection', (done) => {
+        execute(['select current_user'], (rows)=>{
+            expect(rows[0].current_user).to.equal('postgres');
+            done();
+        });
+    });
+    it('accepts a single statement without parameter', (done) => {
+        execute('select current_user', (rows)=>{
+            expect(rows[0].current_user).to.equal('postgres');
             done();
         });
     });
